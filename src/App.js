6fccab92b6c0a5c3d10f59f920cf4777fc41281b
JamesEliasj <119768220+JamesEliasj@users.noreply.github.com>
2024-04-02 17:53:16 -0400

    COUNTER FUNCTION

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import Counter from './file/count/Counter';
+import { React, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => {
+    setCounter((count) => count + 1);
+  };
+
+  const decrease = () => {
+    setCounter((count) => count - 1);
+  };
   return (
-    <div className="App">
-      <Counter/>
+    <div className="counter">
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={decrease}>
+          -
+        </button>
+        <button className="control__btn" onClick={increase}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
